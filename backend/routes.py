e152f987e6f0a7ad13cdbfc9abcf2e3b2cbb48ba
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-11-15 04:44:16 +0530

    added responses architechture

--- backend/routes.py
+++ backend/routes.py
@@ -5,15 +5,17 @@ from api.org import router as organization_router
 from api.dept import router as department_router
 from api.user import router as user_router
 from api.post import router as post_router
+from api.response import router as response_router
 
 # Create FastAPI instance
 app = FastAPI()
 
 # Include Routers
 app.include_router(organization_router)
-app.include_router(department_router)
-app.include_router(user_router)
-app.include_router(post_router)
+app.include_router(department_router, prefix="/organization")
+app.include_router(user_router, prefix="/organization")
+app.include_router(post_router, prefix="/organization/user")
+app.include_router(response_router, prefix="/organization/post")
 
 # Run FastAPI Application
 if __name__ == "__main__":
