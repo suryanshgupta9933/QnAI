4655f4b102086c959eb56746376e23cfb67de03d
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-11-16 16:35:19 +0530

    service account key from env

--- connection.py
+++ connection.py
@@ -1,4 +1,5 @@
 # Importing Dependencies
+import os
 import logging
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -7,10 +8,13 @@ from firebase_admin import credentials, firestore
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# Path to service account key
+SERVICE_ACCOUNT_KEY = os.getenv("serviceAccountKey", "serviceAccountKey.json")
+
 # Initialize Firebase app
 try:
     if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
+        cred = credentials.Certificate(SERVICE_ACCOUNT_KEY)
         firebase_admin.initialize_app(cred)
     db = firestore.client()
 except Exception as e:
