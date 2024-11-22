86696c1f1e3f52a895261eea381a7a101d10a5fc
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-11-22 18:35:49 +0530

    pinecone connection

--- vectordb/connection.py
+++ vectordb/connection.py
@@ -1,23 +1,25 @@
 # Importing Dependencies
 import os
 import logging
+from dotenv import load_dotenv
 from pinecone.grpc import PineconeGRPC as Pinecone
 
 # Configure Logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# Load environment variables
+load_dotenv()
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+
 # Initialize Pinecone
-def connect_pinecone(PINECONE_API_KEY, INDEX_NAME):
+def connect_pinecone():
     try:
         pinecone = Pinecone(api_key=PINECONE_API_KEY)
-        index = pinecone.Index(INDEX_NAME)
-        if index is not None:
-            logging.info(f"Connected to Pinecone Index: {INDEX_NAME}")
-            return index
-        else:
-            logging.error(f"Failed to connect to Pinecone Index: {INDEX_NAME}")
-            return None
+        index = pinecone.Index(PINECONE_INDEX_NAME)
+        logging.info(f"Connected to Pinecone Index: {PINECONE_INDEX_NAME}")
+        return index        
     except Exception as e:
         logging.error(f"Failed to connect to Pinecone: {e}")
         return None
